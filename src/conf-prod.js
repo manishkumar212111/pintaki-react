14b852a2c159bb5edf5c6fbaaa191fab44eb1cdd
manish kumar <manish.kumar212111@gmail.com>
2019-12-06 23:26:51 +0530

    auth added at home screen

--- src/conf-prod.js
+++ src/conf-prod.js
@@ -1,6 +1,6 @@
 export default {
     baseUrl : "http://pintaki.in/",
     apiBaseUrl : "http://api.pintaki.in/",
-    google_client_id: '140876642215-ig5ct1dg6ri6m0des0oflrhpbvvdeahn.apps.googleusercontent.com',
+    google_client_id: '429657210708-tmmkq17fqucnqoc2kp5gt7uo21em6d47.apps.googleusercontent.com',
     port : 3001          
 }
