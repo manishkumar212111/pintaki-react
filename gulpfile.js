bd42639d982a8c10487ed092319f27f65c3a2fa6
pranjal agarwal <pranjal.agarwal@girnarsoft.com>
2019-12-12 22:32:51 +0530

    gulp added

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,15 @@ var htmlmin = require('gulp-htmlmin');
 var runSequence = require('run-sequence');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
+var postcss = require("gulp-postcss");
+var filtermq = require("postcss-filter-mq");
+ var cssnano = require('cssnano');
+ var cleanCSS = require('gulp-clean-css');
+ var rename = require('gulp-rename');
+ var hash = require('gulp-hash');
+
+
+
 
 // Set the browser that you want to support
 const AUTOPREFIXER_BROWSERS = [
@@ -33,12 +42,74 @@ gulp.task('styles', function () {
       .pipe(gulp.dest('./src/static'))
   });
 
+  gulp.task('sass:prod', function () {
+    return gulp.src([
+            './src/static/scss/**/*.scss',
+        ])
+        .pipe(sass({
+                includePaths: [
+                        './src/static/scss',
+                    ],
+                sync : true
+            }))
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(postcss([cssnano]))
+        .pipe(gulp.dest('./src/static/css'));
+  });
+
+  gulp.task('prod-css', function() {
+    return gulp.src([
+            './src/static/css/main.css',
+            './src/static/css/mobile.css',
+        ])
+        .pipe(cleanCSS({debug: true}, function(details) {
+            console.log( "%s : %s -> %s", details.name, details.stats.originalSize, details.stats.minifiedSize);
+        }))
+        .pipe(hash()) // Add hashes to the files' names
+        .pipe(gulp.dest('./src/static/pwa/css')) // Write the renamed files
+        .pipe(hash.manifest('manifest.json', {
+          deleteOld: true,
+          sourceDir: './src/static/pwa/css'
+        })) // Switch to the manifest file
+        .pipe(gulp.dest('./src/static/pwa/css')); // Write the manifest file
+});
+
+gulp.task('minifycss', function() {
+  return gulp.src([
+          '!./src/static/css/main*.css',
+          '!./src/static/css/mobile.css',
+          
+          './src/static/css/**/*.css',
+      ])
+      .pipe(cleanCSS({debug: true}, function(details) {
+          console.log( "%s : %s -> %s", details.name, details.stats.originalSize, details.stats.minifiedSize);
+      }))
+      .pipe(rename({
+          suffix: '.min'
+      }))
+      .pipe(gulp.dest('./src/static/pwa/css'));
+});
+var options = {
+  regex: /min-width:(\s)*(\s)*768px|min-width:(\s)*(\s)*767px|min-width:(\s)*(\s)*992px|min-width:(\s)*(\s)*992px|min-width:(\s)*(\s)*1024px|min-width:(\s)*(\s)*1100px|min-width:(\s)*(\s)*1200px/,// decides the queries to filter
+  invert: true,          // inverts the regex filter result
+  keepBaseRules: true    // keep the non-media css rules
+};
+gulp.task( "css:mq", function () {
+  gulp.src("./src/static/css/main.css")
+      .pipe( postcss([ filtermq(
+              options
+          ) ]) )
+      .pipe( rename( "mobile.css" ) )
+      .pipe( gulp.dest("./src/static/css/") );
+});
+
   gulp.task('clean', () => del(['dist']));
 
-  gulp.task('default', ['clean'], function () {
+  gulp.task('default', function () {
     runSequence(
-    //  'styles',
-    //   'scripts',
-    //   'pages'
+      'sass:prod',// 'sass-amp' please add this when merge in CD
+      'css:mq',
+      ['minifycss',/*'ampminifycss'*/],
+      'prod-css',
     );
   });
--- src/conf-local.js
+++ src/conf-local.js
@@ -1,6 +1,6 @@
 export default {
     baseUrl : "http://localhost:3000/",
-    apiBaseUrl : "http://localhost:9009/",
+    apiBaseUrl : "http://api.pintaki.in/",
     google_client_id: '429657210708-tmmkq17fqucnqoc2kp5gt7uo21em6d47.apps.googleusercontent.com',
     port : 3000          
 }
